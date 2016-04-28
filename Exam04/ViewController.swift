0782615920e4c35260746ce2674e004e21bbf132
DS-104 S <sds-104@70-12-216-128.pools.spcsdns.net>
2016-04-28 11:07:08 +0900

    Initial Commit

---
+++ Exam04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exam04
+//
+//  Created by SDS-104 on 2016. 4. 28..
+//  Copyright © 2016년 SDS-104. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
